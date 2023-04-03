d605dff492fe4dcc2dda9cf019304cfe0ce4d4f0
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-03 13:09:02 +0300

    Added success_url to index page

--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 
 from manager.forms import TaskForm
@@ -7,9 +8,15 @@ from manager.models import Task
 
 
 def index_view(request):
-    return render(request, "manager/index.html")
+    tasks = Task.objects.all()
+    context = {
+        "tasks": tasks
+    }
+
+    return render(request, "manager/index.html", context=context)
 
 
 class TaskCreateView(LoginRequiredMixin, generic.CreateView):
     model = Task
     form_class = TaskForm
+    success_url = reverse_lazy("manager:index")
