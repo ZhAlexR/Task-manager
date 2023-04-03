e764a190f0db4448fe017115c8400dc901010485
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-03 12:33:04 +0300

    Registered models

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from manager.models import Task, Tag, User
+
+admin.site.register(Tag)
+admin.site.register(Task)
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    pass
